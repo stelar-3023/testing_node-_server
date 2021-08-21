121e70f86e290316e8d0c5a3c4a18b7eab2ea380
Steve Larsen <slarsen-3@att.net>
2021-08-21 18:48:42 -0500

    file structure & set up

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.post("/users", async (req, res) => {
+  const { password, username } = req.body;
+  if (!password || !username) {
+    res.sendStatus(400);
+    return;
+  }
+  res.send({ userId: 0 });
+});
+
+export default app;
---
+++ app.test.js
@@ -0,0 +1,8 @@
+import supertest from "supertest";
+import app from "./app.js";
+
+describe("POST /users", () => {
+  describe("given a username and password", () => {});
+
+  descrcibe("when the username and password is missing", () => {});
+});
